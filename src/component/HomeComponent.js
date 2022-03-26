ffd27c0264af037a6969b9b29909f24d19af725b
sachinthaakesh <60322857+sachinthaakesh@users.noreply.github.com>
2022-03-26 13:04:59 +0530

    Update HomeComponent.js

--- src/component/HomeComponent.js
+++ src/component/HomeComponent.js
@@ -23,7 +23,7 @@ function Guest(){
     return(
         <Card>
             <Link to='/menu'>
-            <CardImg src={'./public/assets/images/home.JPG'} />
+            <CardImg src={'public/assets/images/home.JPG'} />
             </Link>
         </Card>
     );
